9b2eeac865d1c291e7d1ad061486b21618334821
Claire Bullock <bullockcm13@gmail.com>
2020-02-20 19:22:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(too old)
   if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
