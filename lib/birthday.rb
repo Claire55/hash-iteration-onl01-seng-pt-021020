2953f0a26cb3e65ee2698808b699e2586408be08
Claire Bullock <bullockcm13@gmail.com>
2020-02-20 19:28:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def age_appropriate_birthday(amanda)
   birthday_kids each do |kids_name, age|
-    if age =< 12
+    if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts nil
